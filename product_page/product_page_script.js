c5a784d425f2aebd7b187effed7ad59baf407373
jennytoc <toctocanj@gmail.com>
2022-11-02 09:36:32 -0600

    Adding more styling to product page

--- product_page/product_page_script.js
+++ product_page/product_page_script.js
@@ -1,15 +1,4 @@
-// Redirect to product page
-// On click => extract ID
-
 let productId = window.localStorage.getItem("productId");
-// async function generateProduct(productId) {
-//     const response = await fetch(`https://dummyjson.com/products/${productId}`);
-//     const data = await response.json();
-
-//     const para = document.createElement("p")
-//     para.appendChild(document.createTextNode(productId));
-//     product_content.appendChild(para);
-// }
 
 async function generateProduct() {
     const response = await fetch(`https://dummyjson.com/products/${productId}`);
@@ -19,19 +8,21 @@ async function generateProduct() {
     img.src = data.images[0];
     product_img.appendChild(img);
 
-    const category = createItems("p", data.category);
+    const category = createItems("p", data.category, "category");
     const title = createItems("h3", data.title);
-    const rating = createItems("p", data.rating);
-    const price = createItems("p", data.price);
-    const description = createItems("p", data.description);
+    const rating = createItems("p", data.rating, "rating");
+    const price = createItems("p", data.price, "price");
+    const description = createItems("p", data.description, "description");
 
-    product_content.append(category, title, rating, price, description);
-    // window.localStorage.clear();
+    product_text.append(category, title, rating, price, description);
 }
 
-function createItems(tagName, textNode) { 
+function createItems(tagName, textNode, idName) { 
     const element = document.createElement(tagName);
     element.appendChild(document.createTextNode(textNode));
+    if (idName != undefined) {
+        element.setAttribute("id", idName);
+    }
     return element;
 }
 
