88fe5ffa90cf823665632d1b7bede5b32dee4774
Tim Robbins <86779745+Legatus727@users.noreply.github.com>
2022-11-02 08:47:18 -0700

    Alert Functionality of Submit

--- Contact-Us/contactUsScripts.js
+++
@@ -1,9 +0,0 @@
-const contactForm = document.getElementById('contactForm');
-
-contactForm.addEventListener('submit', () => {
-    if (document.getElementById('name').value == "" || document.getElementById('email').value == "" || document.getElementById('message').value == "") {
-        alert("All Fields Required");
-    } else {
-        alert("Form Submitted");
-    }
-});
