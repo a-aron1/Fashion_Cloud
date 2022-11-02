060b344b7d86c3eeaa6f0c0b6ed97f06944c6020
jennytoc <toctocanj@gmail.com>
2022-11-02 09:38:25 -0600

    Added try catch statement to landing/product pages

--- landing_page/landing_page_script.js
+++ landing_page/landing_page_script.js
@@ -1,31 +1,35 @@
 async function generateProducts(categoryName, sectionName) {
-    const response = await fetch(`https://dummyjson.com/products/category/${categoryName}`);
-    const data = await response.json();
-
-    for (product of data.products) {
-        
-        const card = createItems("div","card");
-
-        const img = createItems("img", "card-img-top");
-        img.src = product.thumbnail;
-        
-        const cardBody = createItems("div","card-body");
-
-        const linkProduct = createItems("a", "product-link", product.title);
-        linkProduct.href = "../product_page/product_page.html";
-        linkProduct.setAttribute("id", product.id);
-        linkProduct.addEventListener("click", (e) => {
-            window.localStorage.setItem("productId", e.target.id);
-        })
-
-        const cardTitle = createItems("h5", "card-title");
-        cardTitle.appendChild(linkProduct);
-
-        const cardText = createItems("p", "card-text", `$${product.price}`);
-
-        cardBody.append(cardTitle, cardText);
-        card.append(img, cardBody);
-        sectionName.appendChild(card)
+    try {
+        const response = await fetch(`https://dummyjson.com/products/category/${categoryName}`);
+        const data = await response.json();
+    
+        for (product of data.products) {
+            
+            const card = createItems("div","card");
+    
+            const img = createItems("img", "card-img-top");
+            img.src = product.thumbnail;
+            
+            const cardBody = createItems("div","card-body");
+    
+            const linkProduct = createItems("a", "product-link", product.title);
+            linkProduct.href = "../product_page/product_page.html";
+            linkProduct.setAttribute("id", product.id);
+            linkProduct.addEventListener("click", (e) => {
+                window.localStorage.setItem("productId", e.target.id);
+            })
+    
+            const cardTitle = createItems("h5", "card-title");
+            cardTitle.appendChild(linkProduct);
+    
+            const cardText = createItems("p", "card-text", `$${product.price}`);
+    
+            cardBody.append(cardTitle, cardText);
+            card.append(img, cardBody);
+            sectionName.appendChild(card)
+        }
+    } catch (error) {
+        console.error(error)
     }
 }
 
