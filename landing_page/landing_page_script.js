ac689c931976fad85d85ad564b22c174d1ad9daf
jennytoc <toctocanj@gmail.com>
2022-11-01 21:01:18 -0600

    Refactored generateProducts function

--- landing_page/landing_page_script.js
+++ landing_page/landing_page_script.js
@@ -4,37 +4,39 @@ async function generateProducts(categoryName, sectionName) {
 
     for (product of data.products) {
         
-        const card = document.createElement("div");
-        card.classList.add("card");
-        
-        const img = document.createElement("img");
-        img.classList.add("card-img-top");
+        const card = createItems("div","card");
+
+        const img = createItems("img", "card-img-top");
         img.src = product.thumbnail;
         
-        const cardBody = document.createElement("div");
-        cardBody.classList.add("card-body");
-        
-        const linkProduct = document.createElement("a");
+        const cardBody = createItems("div","card-body");
+
+        const linkProduct = createItems("a", "product-link", product.title);
         linkProduct.href = "../product_page/product_page.html";
-        linkProduct.appendChild(document.createTextNode(product.title));
         linkProduct.setAttribute("id", product.id);
         linkProduct.addEventListener("click", (e) => {
             window.localStorage.setItem("productId", e.target.id);
         })
 
-        const cardTitle = document.createElement("h5");
-        cardTitle.classList.add("card-title");
+        const cardTitle = createItems("h5", "card-title");
         cardTitle.appendChild(linkProduct);
 
-        const cardText = document.createElement("p");
-        cardText.classList.add("card-text");
-        cardText.appendChild(document.createTextNode(`$${product.price}`));
+        const cardText = createItems("p", "card-text", `$${product.price}`);
 
         cardBody.append(cardTitle, cardText);
         card.append(img, cardBody);
         sectionName.appendChild(card)
     }
-} 
+}
+
+function createItems(tagName, className, textNode) { 
+    const element = document.createElement(tagName);
+    element.classList.add(className);
+    if (textNode != undefined) {
+        element.appendChild(document.createTextNode(textNode));
+    }
+    return element;
+}
 
 generateProducts("tops", trending);
 generateProducts("mens-shirts", mens_clothing);
