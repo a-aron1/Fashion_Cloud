19494079fc6a657d82be8b0506c4ff9c713b05e8
Tim Robbins <86779745+Legatus727@users.noreply.github.com>
2022-11-02 07:52:46 -0700

    Rebase

--- Contact-Us/contactUsScripts.js
+++ Contact-Us/contactUsScripts.js
@@ -1,11 +1,9 @@
- function checkMessage() {
+const contactForm = document.getElementById('contactForm');
+
+contactForm.addEventListener('submit', () => {
     if (document.getElementById('name').value == "" || document.getElementById('email').value == "" || document.getElementById('message').value == "") {
         alert("All Fields Required");
     } else {
         alert("Form Submitted");
     }
- }
-
-const contactFrom = document.getElementById('contactForm');
-
-FormData.addEventListener('submit', checkMessage);
+});
